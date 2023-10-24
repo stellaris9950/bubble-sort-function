daa2ebe8b1435a6f80416d14f2827ca7a7f24c23
a.wei4 <andyzihengwei@gmail.com>
2023-10-24 13:29:59 -0600

    ;uiohiouhlkjhlkjhl

---
+++ main.py
@@ -0,0 +1,14 @@
+
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog", "at", "good", "eye", "cat", "ball", "fish"]
+
+def bubbleSort(anArray):
+    if type(anArray[1]) == "num":
+        print("integer")
+    else:
+        print("string")
+
+
+type
+
+bubbleSort(words)
