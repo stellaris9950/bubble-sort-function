97b7f0e092fcb5f2a9a3075bbb4f124286b03a01
a.wei4 <andyzihengwei@gmail.com>
2023-10-27 13:30:18 -0600

    finish bubble sort

--- main.py
+++ main.py
@@ -2,13 +2,36 @@
 nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
 words = ["dog", "at", "good", "eye", "cat", "ball", "fish"]
 
-def bubbleSort(anArray):
-    if type(anArray[1]) == "num":
-        print("integer")
-    else:
-        print("string")
 
 
-type
+# elements = [39, 12, 18, 85, 72, 10, 2, 18]
+#
+#
+# def bubbleSort(anArray):
+#
+#     for n in range(len(anArray) - 1):
+#         for i in range(len(anArray) - n - 1):
+#             if anArray[i] > anArray[i + 1]:
+#                 anArray[i], anArray[i + 1] = anArray[i + 1], anArray[i]
+#
+#     return
+#
+#
+# print("Unsorted list is,")
+# print(elements)
+# bubble(elements)
+# print("Sorted Array is, ")
+# print(elements)
 
-bubbleSort(words)
+elements = [39, 12, 18, 85, 72, 10, 2, 18]
+
+
+def selectionSort(anArray):
+    
+
+
+print("Unsorted list is,")
+print(elements)
+bubble(elements)
+print("Sorted Array is, ")
+print(elements)
