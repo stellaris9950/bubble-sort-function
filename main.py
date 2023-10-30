5fb6d93b8adab66f13fc088704cee8eef8386cc4
a.wei4 <andyzihengwei@gmail.com>
2023-10-30 13:30:23 -0600

    finish bubble sort

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 
-nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+nums = [10, 30, 40, 45, 70, 80, 85, 90, 100]
 words = ["dog", "at", "good", "eye", "cat", "ball", "fish"]
 
+elements = [39, 12, 18, 85, 72, 10, 2, 18]
 
+# def linearSearch(anArray, search_item):
+#     for item in anArray:
+#         if item == search_item:
+#
+#             return anArray.index(item)
+#
+#     return -1
+# print(linearSearch(elements, 4564))
+
+
+def binarySearch(anArray, item):
+    lower_index = 0
+    higher_index = len(anArray)
+    check = True
+    while check:
+        middle_index = ( lower_index + higher_index ) // 2
+        if item == anArray[middle_index]:
+            return middle_index
+        elif item < anArray[middle_index]:
+            higher_index = middle_index - 1
+        elif item > anArray[middle_index]:
+            lower_index = middle_index + 1
+    return -1
+
+
+print(binarySearch(nums, 1))
 
 # elements = [39, 12, 18, 85, 72, 10, 2, 18]
 #
@@ -23,15 +50,14 @@ words = ["dog", "at", "good", "eye", "cat", "ball", "fish"]
 # print("Sorted Array is, ")
 # print(elements)
 
-elements = [39, 12, 18, 85, 72, 10, 2, 18]
 
 
-def selectionSort(anArray):
-    
-
-
-print("Unsorted list is,")
-print(elements)
-bubble(elements)
-print("Sorted Array is, ")
-print(elements)
+# def selectionSort(anArray):
+#
+#
+#
+# print("Unsorted list is,")
+# print(elements)
+# bubble(elements)
+# print("Sorted Array is, ")
+# print(elements)
